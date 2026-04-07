be00bca17c31d6092e0820fcee77e3b4819ac0a2
minhvo2025 <minhwuo@gmail.com>
2026-04-07 08:36:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -147,7 +147,7 @@ window.OUTRA_3D_CONFIG = {
     glb: 'docs/Objects/floor.glb',
     yOffset: -6,
     opacity: 1,
-    lockRotationX: Math.PI / 2,
+    lockRotationX: 0,
     lockRotationY: 0,
     lockRotationZ: 0,
   },
